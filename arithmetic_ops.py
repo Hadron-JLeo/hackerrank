a3e4883912fad7021cbb0d6c172dc5cabf8210ab
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2024-07-21 11:38:01 +0200

    Create arithmetic_ops.py

---
+++ arithmetic_ops.py
@@ -0,0 +1,24 @@
+def line_one(a,b):
+    # Add
+    
+    return a+b
+
+def line_two(a,b):
+    # Subtract
+    
+    return a-b
+
+def line_three(a,b):
+    # Multiply
+    
+    return a*b
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(line_one(a,b))
+    print(line_two(a,b))
+    print(line_three(a,b))
+        
