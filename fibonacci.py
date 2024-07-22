177a00f8dd0e94d8f3a23ccde5fa654c85fbb0f6
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2024-07-22 21:30:34 +0200

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+def fibo(n):
+  # Add the last two values together recursively to gain the next
+  # start at 1
+
+  if n in [1,2]:
+    return 1
+  
+  return fibo(n-1) + fibo(n-2)
+
+
+if __name__ == "__main__":
+  print(fibo(3))
